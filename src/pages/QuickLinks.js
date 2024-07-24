ed221303c1cc0859002311d24449426cbaeb4c9b
Astha Samaddar <95849912+Asttttha@users.noreply.github.com>
2024-07-25 01:59:49 +0530

    quickLinks.js

--- src/pages/QuickLinks.js
+++ src/pages/QuickLinks.js
@@ -3,9 +3,9 @@ import React from 'react'
 const QuickLinks = () => {
     return (
         <div className='m-5 md:flex '>
-            <div className='md:w-2/4 h-screen md:h-auto w-full flex md:m-5 p-6 relative'>
-                <img src='https://via.placeholder.com/400' alt='schoolimg' className='rounded md:h-80 h-80 w-72 md:w-2/4 absolute md:left-4 left-0 top-0 ' />
-                <div className='rounded bottom-20 absolute md:right-0 bg-blue-950/100 md:bottom-0 md:w-2/4'>
+            <div className='md:w-2/4 md:h-auto w-full flex md:flex-row flex-col md:m-5 p-2 md:p-6 md:relative'>
+                <img src='https://via.placeholder.com/400' alt='schoolimg' className='rounded w-full md:h-80 h-80 md:w-2/4 md:absolute md:left-4 left-0 top-0 ' />
+                <div className='rounded md:bottom-20 md:absolute md:right-0 bg-blue-950/100 md:bottom-0  md:w-2/4'>
                     <p className='p-2 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et ipsum at turpis ultricies pellentesque. Ut ut aliquet ligula, id consequat ipsum. Integer id auctor lacus. Duis convallis id nisi ut pretium. Etiam semper et enim quis faucibus. Sed sed sollicitudin tellus, sit amet iaculis ligula. Aenean eget arcu sed velit vulputate sollicitudin. Morbi sed erat iaculis, congue leo id, faucibus enim. Nullam sollicitudin eu arcu ac posuere. Nam eget est et nulla varius malesuada in quis nibh.</p></div>
             </div>
             <div className='md:px-10 flex md:m-5 flex-col md:w-2/4 items-center bg-white rounded'>
